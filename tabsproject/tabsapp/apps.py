12b92b9e9e85234d276b57d8ade87abc5b28b91d
Matt Megarry <mattmegarry@gmail.com>
2021-12-21 20:27:34 +0000

    Home page shows a message from us

--- tabsproject/tabsapp/apps.py
+++ tabsproject/tabsapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TabsProjectConfig(AppConfig):
+class TabsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tabs_project'
+    name = 'tabsapp'
