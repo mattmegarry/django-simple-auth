4d0cb299a3582faa861bfcf0cdacf7f5c07381a2
Matt Megarry <mattmegarry@gmail.com>
2021-12-22 18:36:21 +0000

    Rename project

--- djangosimpleauth/djangosimpleauth/asgi.py
+++ djangosimpleauth/djangosimpleauth/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tabsproject project.
+ASGI config for djangosimpleauth project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabsproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosimpleauth.settings')
 
 application = get_asgi_application()
--- djangosimpleauth/manage.py
+++ djangosimpleauth/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabsproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'djangosimpleauth.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
